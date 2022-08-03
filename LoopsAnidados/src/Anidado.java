cac53c85bea86817ff0621321448defb34701610
daniel-bl <danielinformatica457@gmail.com>
2022-08-03 13:25:25 -0500

    modificación 3

--- LoopsAnidados/src/Anidado.java
+++ LoopsAnidados/src/Anidado.java
@@ -1,17 +1,17 @@
 
 public class Anidado {
 	public static void main(String[] args) {
-		int[][] list = {{1,13},{5,2},{2,2},{2,4}};
+		int[][] list = {{1,13},{5,2},{2,2},{1,6,2,4}};
 		int searchValue = 2;
 		int positionX = -1;
 		int positionY = -1;
 		
 		PARENT_LOOP: for(int i=0; i<list.length; i++) {
-			for(int j=0; j<list[i].length; j++) {
+			INNER_LOOP: for(int j=0; j<list[i].length; j++) {
 					if(list[i][j]==searchValue) {
 						positionX = i;
 						positionY = j;
-						break;
+						break INNER_LOOP;
 					}
 			}
 		}
